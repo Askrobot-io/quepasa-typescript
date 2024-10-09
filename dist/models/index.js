f574ad180a04d08c95165bb8421d63370a2026fd
yuriy-vorontsov <yuriy.vorontsov@yandex.ru>
2024-10-09 16:57:22 +0300

    build dist

---
+++ dist/models/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+/* eslint-disable */
+__exportStar(require("./AnswerDetail"), exports);
+__exportStar(require("./AnswerDetailData"), exports);
+__exportStar(require("./AnswerDetailDataLabeledLinksValue"), exports);
+__exportStar(require("./AnswerDetailDataLinksValue"), exports);
+__exportStar(require("./BatchStatus"), exports);
+__exportStar(require("./BatchStatusData"), exports);
+__exportStar(require("./ChunksDetail"), exports);
+__exportStar(require("./ChunksDetailDataInner"), exports);
+__exportStar(require("./CreatedBatchStatus"), exports);
+__exportStar(require("./CreatedBatchStatusData"), exports);
+__exportStar(require("./Document"), exports);
+__exportStar(require("./DocumentDetail"), exports);
+__exportStar(require("./DocumentDetailData"), exports);
+__exportStar(require("./DocumentDetailDataPagesInner"), exports);
+__exportStar(require("./DocumentNotFound"), exports);
+__exportStar(require("./DocumentPagesInner"), exports);
+__exportStar(require("./OperationFailedStatus"), exports);
+__exportStar(require("./RetrieveAnswerRequest"), exports);
+__exportStar(require("./RetrieveAnswerRequestUserInfo"), exports);
+__exportStar(require("./SetupTelegramRequest"), exports);
+__exportStar(require("./SetupTelegramRequestCommands"), exports);
+__exportStar(require("./SetupTelegramRequestCommandsAsk"), exports);
+__exportStar(require("./SetupTelegramRequestCommandsStart"), exports);
+__exportStar(require("./TelegramStatus"), exports);
